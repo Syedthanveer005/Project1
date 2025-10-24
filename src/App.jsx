a66c79fa5806ecc485652528d43f04f5d2bb8f1e
Syedthanveer005 <syedthanveer005@gmail.com>
2025-10-24 12:40:04 +0530

    Updated home page design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { useEffect } from "react";
 import Home from './Router/Home';
 import Education from './Router/Education';
 import Service from './Router/Service';
@@ -6,6 +7,19 @@ import Contact from './Router/Contact';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const menu = document.querySelector(".menu");
+      if (window.scrollY > 50) {
+        menu.classList.add("shrink");
+      } else {
+        menu.classList.remove("shrink");
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div>
       <BrowserRouter>
